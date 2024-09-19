97fce73c624de547d1fde4515785925cf895a8a2
Marco Antonio Gelde <marcogelde@hotmail.com>
2024-09-19 01:15:38 -0300

    Ex5

---
+++ src/exerciciosa2/Ex6.java
@@ -0,0 +1,15 @@
+package exerciciosa2;
+
+import java.util.Scanner;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite o numero de horas: ");
+        int horas = leitor.nextInt();
+        int segundos = horas * 3600;
+
+        System.out.println(horas + " horas são equivalentes a " + segundos + " segundos.");
+        leitor.close();
+    }
+}
