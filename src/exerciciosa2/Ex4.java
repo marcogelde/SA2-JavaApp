62975653edd7eb57a13d06d74cd314ed75c3a3a9
Marco Antonio Gelde <marcogelde@hotmail.com>
2024-09-19 00:58:23 -0300

    Ex4

---
+++ src/exerciciosa2/Ex4.java
@@ -0,0 +1,12 @@
+package exerciciosa2;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        desenharLinha(10);
+    }
+    public static void desenharLinha(int tamanho) {
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print("_");
+        }
+    }
+}
